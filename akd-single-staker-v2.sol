83a0230b6f632744468d8e3d357501a68db9309d
leejh16 <cheny5dyh@gmail.com>
2022-04-10 21:15:15 +0900

    callable single staker-20220410

--- akd-single-staker-v2.sol
+++ akd-single-staker-v2.sol
@@ -25,11 +25,11 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 contract Staker { // is ERC20 
-	mapping ( address => uint256 ) _balances ; // holder => balance , different sources pooled together due to stable nature
+	mapping ( address => uint256 ) public _balances ; // holder => balance , different sources pooled together due to stable nature
 	mapping ( address => uint256 ) _rewards ; 
-	mapping ( address => uint256 ) _last_deposit_time ; 
-	mapping ( address => uint256 ) _last_withdraw_time ; 
-	mapping ( address => uint256 ) _last_claim_time ; 
+	mapping ( address => uint256 ) public _last_deposit_time ; 
+	mapping ( address => uint256 ) public _last_withdraw_time ; 
+	mapping ( address => uint256 ) public _last_claim_time ; 
 	address public _owner ;
 	mapping ( address => bool ) public _admins ;
 	uint256 public _min_stake_amount ;
@@ -38,9 +38,16 @@ contract Staker { // is ERC20
 	uint256 public _mint_balance_to_qualify_for_claim ;
 	uint256 public _reward_rate ;
 	uint256 public _giveaway_amount_per_day = 28109589 * 10 ** 14 ; // 2810.9589 == 1_026_000 * 10 ** 18 / 365 ; 
-	address [] public _payable_addresses ;
-	mapping ( address => uint256 ) public _payables_address_uint ;
-	mapping ( uint256 => address ) public _payables_uint_address ;
+	/******** */
+	address [] public _arr_payable_addresses ;
+	mapping ( address => uint256 ) public _map_payables_address_to_idx ;
+	mapping ( uint256 => address ) public _map_payables_idx_to_address ;
+	/******** */
+	address [] public _arr_qualified_addresses ;
+//	uint256 [] public _arr_qualified_balances ;
+	mapping ( address => uint256 ) public _map_qualified_address_balance ;
+//	mapping ( address => uint256 ) public _qualified_address_to_idx ;
+	//mapping ( uint256 => address ) public _qualified_i dx_to_address ;
 	constructor ( //  address __admin , 
 		address __token_from // to stake
 		, address __token_to // reward
@@ -60,24 +67,55 @@ contract Staker { // is ERC20
 		require ( _address == _owner , "ERR() only owner");
 		_;
 	}
-	function query_recipients_and_principal_amount ( uint256 _ref_timepoint , uint256 _timewindow_len ) public returns ( address [] memory , uint256 principal_amount ) {
-		uint256 timenow = _ref_timepoint ;
+	modifier onlyowner_or_admin ( address _address) {
+		require ( _address == _owner || _admins[_address] , "ERR() not privileged" );
+		_ ;
+	}
+	function query_recipients_and_principal_amount ( uint256 _ref_timepoint , uint256 _timewindow_len ) 
+		public view returns ( address [] memory , uint256 principal_amount ) {
+//		uint256 timenow = _ref_timepoint ;
 		uint256 time_period_start = _ref_timepoint - _timewindow_len ;
 		uint256 principal_amount = 0 ;
 		address [] memory recipients ;
-		for ( uint256 idx = 0 ; idx < _payable_addresses.length ; idx ++ ) {
-			address recipient = _payable_addresses [ idx ] ;
-			if ( _last_claim_time [ recipient ] >= time_period_start 
+		for ( uint256 idx = 0 ; idx < _arr_payable_addresses.length ; idx ++ ) {
+			address recipient = _arr_payable_addresses [ idx ] ;
+			if (	_last_claim_time [ recipient ] >= time_period_start 
 				|| _last_deposit_time[ recipient ] >= time_period_start
 			 	|| _last_withdraw_time[ recipient ] >= time_period_start			 ){
 					 continue ;}
 			else { principal_amount += _balances[ recipient ] ; }
-			recipients.push ( recipient );
+            recipients [ idx ] = recipient ;
+//			recipients.push ( recipient );
 		}
 		return ( recipients , principal_amount );
 	}
-	function settle ( uint256 _ref_timepoint , uint256 _timewindow_len ) public {
-		address recipients;
+	function reinit_qualified () public onlyowner_or_admin (msg.sender ){
+		uint256 N=_arr_qualified_addresses.length ;
+		if( N>0){}
+		else {return ; }
+		for ( uint256 i=0; i<N; i++){
+			address recipient = _arr_qualified_addresses [ i ] ;
+			_map_qualified_address_balance [ recipient ] = 0 ;
+		}
+		delete _arr_qualified_addresses ;
+//		delete _arr_qualified_bal ances ;
+	}
+	function settle_ver_reflect_to_qualified ( uint256 _ref_timepoint , uint256 _timewindow_len ) public {
+		address [] memory recipients ;
+		uint256 principal_amount ;
+		( recipients , principal_amount ) = query_recipients_and_principal_amount ( _ref_timepoint , _timewindow_len ) ;
+		for ( uint256 idx =0 ; idx <recipients.length ; idx ++ ){
+			address recipient = recipients [ idx ];
+			uint256 amounttogive = _giveaway_amount_per_day * _balances[ recipient ] / principal_amount ; 
+			_arr_qualified_addresses[ idx ] = recipient ;
+//			_arr_qualified_bala nces [ idx ] = amounttogive ;
+			_map_qualified_address_balance [ recipient ] = amounttogive; 
+//			_qualified_address_to_idx [ recipient ] = idx ;
+	//		_qualified_idx _to_address [ idx ] = recipient ;
+		}
+	}
+	function settle_direct_pay ( uint256 _ref_timepoint , uint256 _timewindow_len ) public {
+		address [] memory recipients;
 		uint256 principal_amount;
  	 	( recipients , principal_amount ) = query_recipients_and_principal_amount ( _ref_timepoint , _timewindow_len ) ;
 		for ( uint256 idx = 0; idx<recipients.length ; idx ++ ){
@@ -99,9 +137,9 @@ contract Staker { // is ERC20
 		IERC20 ( _token_from ).transferFrom ( msg.sender , address( this ) , _amount ) ;
 		_balances[ _to ] += _amount ;
 		if ( _last_deposit_time [ _to] == 0 ) {
-			_payable_addresses.push ( _to ) ;
-			_payables_address_uint [ _to ] = _payable_addresses.length - 1 ;
-			_payables_uint_address [ _payable_addresses.length - 1 ] = _to ;
+			_arr_payable_addresses.push ( _to ) ;
+			_map_payables_address_to_idx [ _to ] = _arr_payable_addresses.length - 1 ;
+			_map_payables_idx_to_address [ _arr_payable_addresses.length - 1 ] = _to ;
 		} else {
 		}
 		_last_deposit_time [ _to ] = block.timestamp ;
@@ -115,7 +153,10 @@ contract Staker { // is ERC20
 		, address _to
 		, uint256 amount
 	) ;
-	function query_claimable_amount ( address _address ) public view returns ( uint ){
+	function query_claimable_amount ( address _address ) public view returns ( uint256 ) {
+		return _map_qualified_address_balance [ _address] ;
+	}
+	function query_claimable_amount_ver_does_not_consider_daily_cap ( address _address ) public view returns ( uint ){
 		uint256 timedelta ;
 		if ( _last_claim_time[ _address ] == 0 ){ // has never claimed
 			timedelta = block.timestamp - _last_deposit_time [ _address];
@@ -141,8 +182,8 @@ contract Staker { // is ERC20
 	function query_claimable_time_from_now ( address _address ) public view returns ( uint256 ) {
 		uint256 max = mathmax2 ( _last_claim_time[ _address] , _last_deposit_time [ _address ] );
 		max = mathmax2 ( max , _last_withdraw_time[ _address ]) ;
-		if ( max == 0 ){return 0 ; }
-		if ( block.timestamp >= max + 3600 *24 ){return 0 ; }
+		if ( max == 0 ){ return 0 ; }
+		if ( block.timestamp >= max + 3600 *24 ){ return 0 ; }
 		else {
 			return block.timestamp - max - 3600 * 24 ;
 		}
@@ -151,27 +192,39 @@ contract Staker { // is ERC20
 		uint256 _amount 
 		, address _to
 	) public {
-		if (_balances[ msg.sender ]< _amount ){revert("ERR() balance not enough");}
+		if (_balances[ msg.sender ]< _amount ){revert( "ERR() balance not enough" );}
 		else {}
 		IERC20( _token_from).transfer ( _to , _amount ) ;
 		_balances[ msg.sender ] -= _amount ;
 		_last_withdraw_time [ msg.sender ] = block.timestamp ;
 		if ( _balances [ msg.sender ] > 0 ) {
 		} else {
-			uint256 idx = _payables_address_uint [ msg.sender ] ;
-			_payable_addresses [ idx ] = _payable_addresses.pop ();
-			_payables_uint_address [ idx ] = _payable_addresses [ idx ] ; // _payables_uint_address [ _payable_addresses.length -1 ] ;
-			_payables_address_uint [ _payable_addresses [ idx ] ] = idx ;
+			uint256 idx = _map_payables_address_to_idx [ msg.sender ] ;
+			address slot_taker = _arr_payable_addresses[ _arr_payable_addresses.length -1 ];
+             _arr_payable_addresses.pop ();
+			_arr_payable_addresses [ idx ] = slot_taker ;
+			_map_payables_idx_to_address [ idx ] = slot_taker ; // _arr_payable_addresses [ idx ] ; // _map_payables_idx_to_address [ _arr_payable_addresses.length -1 ] ;
+			_map_payables_address_to_idx [ slot_taker ] = idx ;
 		}
 	}
-	function claim (
+	function claim ( address _to ) public returns (bool) {
+		uint256 claimableamount = query_claimable_amount ( msg.sender ) ;
+		if ( claimableamount >0 ) {}
+		else {return false ;} // revert ("ERR() none claimable"); }
+		IERC20 ( _token_to ).transfer ( _to , claimableamount ) ;
+		_map_qualified_address_balance [ msg.sender ] = 0 ;
+		emit Claimed ( msg.sender , _to , claimableamount ); 
+		return true ;
+	}
+
+	function claim_ver_does_not_consider_daily_cap (
 		address _to
 	) public {
 		require ( _balances[msg.sender ] >= _mint_balance_to_qualify_for_claim , "ERR() does not meet min balance" ) ;
 //		require ( _rewards[ msg.sender]>0 , "ERR() none claimable" );
 		uint256 timedelta ;
 		if ( _last_claim_time[ msg.sender ] == 0 ){ // has never claimed
-			timedelta = block.timestamp - _last_deposit_time [ msg.sender];
+			timedelta = block.timestamp - _last_deposit_time [ msg.sender ];
 		}
 		else {
 			timedelta = block.timestamp - _last_claim_time [ msg.sender ];
@@ -186,10 +239,21 @@ contract Staker { // is ERC20
 		_last_claim_time[ msg.sender ] = block.timestamp ;
 		emit Claimed ( msg.sender , _to , amounttogive );
 	}
-    function mybalance ( address _token ) public returns ( uint256 ) {
-        return IERC20(_token).balanceOf ( address (this ));
-    }
-    function allowance ( address _token , address _holder ) public returns ( uint256 ) {
-        return IERC20( _token).allowance ( _holder , address(this ));
-    }
+	function set_claimable_amount ( address _address , uint256 _amount ) public onlyowner_or_admin ( msg.sender ) {
+		if ( _map_qualified_address_balance [ _address ] == 0 ){
+			_arr_qualified_addresses.push ( _address ) ;
+			_map_qualified_address_balance [ _address ] = _amount ;
+		} else {
+			_map_qualified_address_balance [ _address ]  = _amount ;
+		}
+	} 
+	function withdraw_fund ( address _tokenaddress , uint256 _amount , address _to ) public onlyowner (msg.sender ) {
+  	IERC20(_tokenaddress).transfer ( _to , _amount );
+  }
+	function mybalance ( address _token ) public view returns ( uint256 ) {
+			return IERC20(_token).balanceOf ( address (this ));
+	}
+	function allowance ( address _token , address _holder ) public view returns ( uint256 ) {
+			return IERC20( _token).allowance ( _holder , address(this ));
+	}
 }
